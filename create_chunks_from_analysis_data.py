e674a54c23414055b1bad2cc15ad09ad0caf33a0
sterlingsomers <sterlingsomers@gmail.com>
2018-02-14 14:01:04 -0500

    A new algorithm that tries to maximize distance. It isn't perfect because it only compares against harvested vectors. I'd have to re-write to get the maximally distant set.

--- create_chunks_from_analysis_data.py
+++ create_chunks_from_analysis_data.py
@@ -25,49 +25,139 @@ green_orange_non = 0
 green_orange_blocking = 0
 viables = []
 history.reverse()
-for case in history:
-    if case['green'] and not case['orange'] and not case['blocking']:
-        if current == 'green_only':
-            #try and avoid multiples of same scenario
-            continue
-        if green_only >= 5:
-            continue
-        print("green only")
-        green_only += 1
-        current = 'green_only'
-        viables.append(case)
-    elif case['green'] and case['orange'] and case['blocking']:
-        if current == 'green_orange_blocking':
-            # try and avoid multiples of same scenario
-            continue
-        if green_orange_blocking >= 5:
-            continue
-        print("blocking")
-        green_orange_blocking += 1
-        current = 'green_orange_blocking'
-        viables.append(case)
-    elif case['green'] and case['orange'] and not case['blocking']:
-        if current == 'green_orange_non':
-            # try and avoid multiples of same scenario
-            continue
-        if green_orange_non >= 5:
-            continue
-        print("non-blocking")
-        green_orange_non += 1
-        current = 'green_orange_non'
-        viables.append(case)
-    elif not case['green'] and case['orange'] and not case['blocking']:
-        if current == 'orange_only':
-            # try and avoid multiples of same scenario
-            continue
-        if orange_only >= 5:
-            continue
-        print("orange_only")
-        orange_only += 1
-        current = 'orange_only'
-        viables.append(case)
+minimum_distance = 100
+step = 1
+max_cases = 20
 
-print("g",green_only,"o",orange_only,"gob",green_orange_blocking,"gon",green_orange_non)
+while minimum_distance >= 0:
+    for case in history:
+        number_same_cases = 0
+        smallest_distance = 10000
+        case_distance = 100000
+        for viable in viables:
+            if viable['green'] == case['green'] and \
+                viable['orange'] == case['orange'] and \
+                viable['blocking'] == case['blocking']:
+                number_same_cases += 1
+                case_distance = np.linalg.norm(viable['fc1']-case['fc1'])
+            if case_distance <= smallest_distance:
+                smallest_distance = case_distance
+        if number_same_cases <= (max_cases-1) and smallest_distance >= minimum_distance:
+            print("adding case g",case['green'],'o',case['orange'],'b',case['blocking'],'min distance',smallest_distance)
+            viables.append(case)
+            continue
+        if number_same_cases == 0:
+            print("adding case g", case['green'], 'o', case['orange'], 'b', case['blocking'],'min disancce',smallest_distance)
+            viables.append(case)
+            continue
+
+    minimum_distance -= 1
+
+# while minimum_distance >= 0:
+#     minimum_distance -= 1
+#     for case in history:
+#         if case['green'] and not case['orange'] and not case['blocking']:
+#             if current == 'green_only':
+#                 #try and avoid multiples of same scenario
+#                 continue
+#             if green_only >= 5:
+#                 continue
+#             print("green only")
+#             green_only += 1
+#             current = 'green_only'
+#             for viable in viables:
+#                 if viable['green'] and not viable['orange'] and not viable['blocking']:
+#                     if np.linalg.norm(viable['fc1'] - case['fc1']) >= minimum_distance:
+#                         #ed = np.linalg.norm(narray1 - narray2)
+#                         print("adding green distance", np.linalg.norm(viable['fc1']-case['fc1']))
+#                         viables.append(case)
+#             if not viables:
+#                 viables.append(case)
+#             has_case = False
+#             for viable in viables:
+#                 if viable['green'] and not viable['orange'] and not viable['blocking']:
+#                     has_case = True
+#                     break
+#             if not has_case:
+#                 print("adding green only")
+#                 viables.append(case)
+#         elif case['green'] and case['orange'] and case['blocking']:
+#             if current == 'green_orange_blocking':
+#                 # try and avoid multiples of same scenario
+#                 continue
+#             if green_orange_blocking >= 5:
+#                 continue
+#             print("blocking")
+#             green_orange_blocking += 1
+#             current = 'green_orange_blocking'
+#             for viable in viables:
+#                 if viable['green'] and viable['orange'] and viable['blocking']:
+#                     if np.linalg.norm(viable['fc1'] - case['fc1']) >= minimum_distance:
+#                         #ed = np.linalg.norm(narray1 - narray2)
+#                         print("adding blocking distance", np.linalg.norm(viable['fc1'] - case['fc1']))
+#                         viables.append(case)
+#             if not viables:
+#                 viables.append(case)
+#             has_case = False
+#             for viable in viables:
+#                 if viable['green'] and viable['orange'] and viable['blocking']:
+#                     has_case = True
+#                     break
+#             if not has_case:
+#                 print("adding blocking")
+#                 viables.append(case)
+#         elif case['green'] and case['orange'] and not case['blocking']:
+#             if current == 'green_orange_non':
+#                 # try and avoid multiples of same scenario
+#                 continue
+#             if green_orange_non >= 5:
+#                 continue
+#             print("non-blocking")
+#             green_orange_non += 1
+#             current = 'green_orange_non'
+#             for viable in viables:
+#                 if viable['green'] and viable['orange'] and not viable['blocking']:
+#                     if np.linalg.norm(viable['fc1'] - case['fc1']) >= minimum_distance:
+#                         # ed = np.linalg.norm(narray1 - narray2)
+#                         print("adding not-blocking distance", np.linalg.norm(viable['fc1'] - case['fc1']))
+#                         viables.append(case)
+#             if not viables:
+#                 viables.append(case)
+#             has_case = False
+#             for viable in viables:
+#                 if viable['green'] and viable['orange'] and not viable['blocking']:
+#                     has_case = True
+#                     break
+#             if not has_case:
+#                 print("adding non-blocking")
+#                 viables.append(case)
+#         elif not case['green'] and case['orange'] and not case['blocking']:
+#             if current == 'orange_only':
+#                 # try and avoid multiples of same scenario
+#                 continue
+#             if orange_only >= 5:
+#                 continue
+#             print("orange_only")
+#             orange_only += 1
+#             current = 'orange_only'
+#             for viable in viables:
+#                 if not viable['green'] and viable['orange'] and not viable['blocking']:
+#                     if np.linalg.norm(viable['fc1'] - case['fc1']) >= minimum_distance:
+#                         # ed = np.linalg.norm(narray1 - narray2)
+#                         print("adding orange distance", np.linalg.norm(viable['fc1'] - case['fc1']))
+#                         viables.append(case)
+#             if not viables:
+#                 viables.append(case)
+#             has_case = False
+#             for viable in viables:
+#                 if not viable['green'] and viable['orange'] and not viable['blocking']:
+#                     has_case = True
+#                     break
+#             if not has_case:
+#                 print("adding orange only")
+#                 viables.append(case)
+
+#print("g",green_only,"o",orange_only,"gob",green_orange_blocking,"gon",green_orange_non)
 
 print("viables",len(viables))
 for case in viables:
